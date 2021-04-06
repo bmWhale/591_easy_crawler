88f4fc5671b0539fce36ea096116846c8eac0c2a
bmWahle.C <bmwhale.c@gmail.com>
2021-04-06 18:54:06 +0800

    [FIX] Add retry 10 times and increase timeout
    - Add retry 10 times for getting empty result form absolute_links
    - Increase timeout to 100 seconds for web page loading issue

--- 591xls.py
+++ 591xls.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import signal
 
 from requests_html import HTMLSession
 from openpyxl import Workbook
@@ -26,8 +27,14 @@ def gather_info(url):
 #    print("詳情：" + re.sub(r"\s+", "", detail))
     ws.append([name, re.sub(r"\s+", "", phone), remark])
 
+def exit(signum, frame):
+        print('You choose to stop me.')
+        exit()
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, exit)
+    signal.signal(signal.SIGTERM, exit)
+
     init_url = 'https://sale.591.com.tw/?shType=list&kind=9&pattern=3&regionid=5&section=54'
     total_num = '1413'
     pages = 48
@@ -39,23 +46,28 @@ if __name__ == '__main__':
     count = 0;
     for i in range(pages):
         url = init_url +'&firstRow='+ str(i*30) + '&totalRows='+total_num
-        print("target URL:", url)
-        s = HTMLSession()
-        r = s.get(url)
-        r.html.render(sleep = 1, timeout = 20)
-        products = r.html.xpath('//*[@id="app"]/div[4]/div[2]/section/div[3]', first=True)
         print('Page: ',i+1,'+++++++++++++++++++++++++++++++')
-        if not products:
-            print("No jobs")
-            continue
-        print("links:",products.absolute_links)
-        for item in products.absolute_links:
-            print("item:", item)
-            if 'sale.591.com.tw' in item:
-                count = count + 1;
-                print('===============================')
-                print("count:",count,item)
-                gather_info(item)
+        print("target URL:", url)
+        for i in range(1,10):
+            s = HTMLSession()
+            r = s.get(url)
+            r.html.render(sleep = 1, timeout = 100)
+            products = r.html.xpath('//*[@id="app"]/div[4]/div[2]/section/div[3]', first=True)
+            if not products:
+                print("No jobs...FIXME!")
+                continue
+            print("Number of links:",len(products.absolute_links))
+            if len(products.absolute_links) > 0:
+                for item in products.absolute_links:
+                    print("item:", item)
+                    if 'sale.591.com.tw' in item:
+                        count = count + 1;
+                        print('===============================')
+                        print("count:",count,item)
+                        gather_info(item)
+                break;
+            else:
+                print('Retry url: ', url, 'again')
     wb.save(excelfile)
 
 
