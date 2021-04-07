6d11a7061488aa2c24aacb6f1b32f8b620f8bef2
bmWahle.C <bmwhale.c@gmail.com>
2021-04-07 20:48:37 +0800

    [NEW] Get house information

--- 591xls.py
+++ 591xls.py
@@ -7,48 +7,70 @@ from requests_html import HTMLSession
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 
-
-def gather_info(url):
-
+def get_total_items(url):
     result = requests.get(url,headers={'User-Agent': 'Chrome/35.0.1916.47'})
     if result.status_code != 200:
         return None, None
     soup1 = BeautifulSoup(result.content, 'html.parser')
-#    addrs = soup1.find_all('span', class_='info-addr-value')
-#    for s in addrs:
-#        print("地址：" + s.text)
+    print('區域：', soup1.find('span', {'class':'areaTxt'}).text)
+    s = HTMLSession()
+    r = s.get(url)
+    r.html.render(sleep = 1, timeout = 100)
+    p = r.html.xpath('/html/body/div[2]/div[2]/div[4]/div[2]/section/div[1]/div[1]', first=True)
+    total_items = 788;
+    total_pages = int(total_items/30 + 1)
+    return total_items, total_pages
+
+def gather_info(ws, url):
+    result = requests.get(url,headers={'User-Agent': 'Chrome/35.0.1916.47'})
+    if result.status_code != 200:
+        return None, None
+    soup1 = BeautifulSoup(result.content, 'html.parser')
+    price = soup1.find('span', class_='info-price-num').text
+    print("總價："+ re.sub(r"\s+", "", price));
+    addrs = soup1.find_all('span', class_='info-addr-value')
+    for s in addrs:
+        print("地址：" + s.text)
+
+    floors_key = soup1.find_all('div', class_='info-floor-key')
+    #floors_value = soup1.find_all('div', class_='info-floor-value')
+    for s in floors_key:
+        print("格局：" + s.text);
+
     name = soup1.find("span", class_="info-span-name").text
     print("姓名：" + soup1.find("span", class_="info-span-name").text)
     phone = soup1.find("span", class_="info-host-word").text
     print("電話：" + re.sub(r"\s+", "", phone))
     remark = soup1.find("span", class_="info-span-msg").text
     print("訊息：" + soup1.find("span", class_="info-span-msg").text)
-#    detail = soup1.find("div", class_="info-detail-show").text
-#    print("詳情：" + re.sub(r"\s+", "", detail))
-    ws.append([name, re.sub(r"\s+", "", phone), remark])
+    detail = soup1.find("div", class_="info-detail-show").text
+    print("詳情：" + re.sub(r"\s+", "", detail))
+    #ws.append([name, re.sub(r"\s+", "", phone), remark])
 
 def exit(signum, frame):
         print('You choose to stop me.')
         exit()
 
-if __name__ == '__main__':
+def main(outputfile, init_url):
     signal.signal(signal.SIGINT, exit)
     signal.signal(signal.SIGTERM, exit)
+    if not init_url or not outputfile:
+        print('parameters error');
+        return
 
-    init_url = 'https://sale.591.com.tw/?shType=list&kind=9&pattern=3&regionid=5&section=54'
-    total_num = '1413'
-    pages = 48
-
-    excelfile = "591.xlsx"
+    print("Set URL:", init_url)
+    items, pages = get_total_items(init_url)
+    if items == 0:
+        return
     wb = Workbook()
     ws = wb.active
     ws.append(['姓名', '電話', '訊息'])
     count = 0;
     for i in range(pages):
-        url = init_url +'&firstRow='+ str(i*30) + '&totalRows='+total_num
+        url = init_url +'&firstRow='+ str(i*30) + '&totalRows='+str(items)
         print('Page: ',i+1,'+++++++++++++++++++++++++++++++')
-        print("target URL:", url)
-        for i in range(1,10):
+        print("Select URL:", url)
+        for j in range(1,10):
             s = HTMLSession()
             r = s.get(url)
             r.html.render(sleep = 1, timeout = 100)
@@ -64,10 +86,16 @@ if __name__ == '__main__':
                         count = count + 1;
                         print('===============================')
                         print("count:",count,item)
-                        gather_info(item)
+                        gather_info(ws, item)
                 break;
             else:
                 print('Retry url: ', url, 'again')
-    wb.save(excelfile)
+    wb.save(output_file_name)
 
-
+if __name__ == '__main__':
+    # -------- configurable parameter -------- #
+    url = "https://sale.591.com.tw/?shType=list&regionid=5&section=54&kind=9&pattern=3&shape=2&price=500$_1500$"
+    output_file_name = '591_output.csv'
+    # ---------------------------------------- #
+    main(output_file_name, url)
+    print('finish!')
